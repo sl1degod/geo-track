d3b7e693cb9f21a642ba6a4eac0ea6d7db0aa72e
sl1degod <sl1degod86@gmail.com>
2024-01-11 15:14:34 +0300

    12

---
+++ src/views/Main.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="main">
+  </div>
+</template>
+
+<script>
+
+import axios from "axios";
+
+export default {
+  name: 'MainView',
+  data() {
+    return {
+      fio: ""
+    }
+  },
+  computed: {
+    firstName() {
+      return this.fio
+    }
+  },
+  components: {
+
+  },
+  methods: {
+    async fetchData() {
+      const res = await axios.get("http://127.0.0.1:5000/users/1")
+      this.fio = res.data
+      console.log(res.data)
+      console.log("123")
+    }
+  },
+  mounted() {
+    // this.fetchData();
+  },
+}
+</script>
+
+<style>
+
+</style>
